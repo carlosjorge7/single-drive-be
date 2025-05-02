cecc77c26d49cb120ff6bb846a02f1013bef1694
Carlos <carlosjorgech7@gmail.com>
2025-05-02 19:39:14 +0200

    fix: cors

--- singledrive/settings.py
+++ singledrive/settings.py
@@ -56,14 +56,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  # ✅ DEBE ir aquí, lo más arriba posible
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
 ]
+
 
 CORS_ORIGIN_ALLOW_ALL = True
 
